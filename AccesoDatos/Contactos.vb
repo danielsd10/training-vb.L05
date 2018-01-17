6b7c812d6d93343654ef0f6f1c14b825d35a3690
Daniel Salas <dsalasd@gmd.com.pe>
2018-01-16 19:30:02 -0500

    proyecto Windows Forms y clases de entidades y acceso a datos

---
+++ AccesoDatos/Contactos.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports Entidades
+
+Public Class Contactos
+
+    Private CadenaConexion As String
+
+    Sub New()
+        CadenaConexion = "Data Source=localhost;Initial Catalog=TrainingNET;Integrated Security=True"
+    End Sub
+
+    Public Function ListarPersonas(Optional strBuscar As String = "") As List(Of Persona)
+        Dim cnx As New SqlConnection
+        Dim cmd As New SqlCommand
+        Dim param As SqlParameter
+
+        Dim lstPersonas As New List(Of Persona)
+        Dim objPersona As Persona
+
+        Try
+            cnx = New SqlConnection(CadenaConexion)
+            cnx.Open()
+
+            cmd = New SqlCommand()
+            cmd.Connection = cnx
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.CommandText = "ListarPersonas"
+
+            If strBuscar <> "" Then
+                param = New SqlParameter("Buscar", SqlDbType.VarChar)
+                param.Value = strBuscar
+                cmd.Parameters.Add(param)
+            End If
+
+            Using reader As SqlDataReader = cmd.ExecuteReader
+                While reader.Read
+                    objPersona = New Persona
+                    objPersona.Id = reader("id_contacto")
+                    objPersona.Nombre = reader("nombre")
+                    objPersona.Apellido = reader("apellido")
+                    objPersona.DNI = reader("dni")
+                    objPersona.Direccion = reader("direccion")
+                    lstPersonas.Add(objPersona)
+                    reader.NextResult()
+                End While
+            End Using
+
+        Catch ex As SqlException
+
+        Catch ex As Exception
+
+        Finally
+            cmd.Dispose()
+            cnx.Dispose()
+            cnx.Close()
+        End Try
+        Return lstPersonas
+    End Function
+
+End Class
---
+++ LibretaForms/Form1.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Configuration
+Imports AccesoDatos
+Imports Entidades
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim c As Contactos
+        Dim lst As List(Of Persona)
+
+        c = New Contactos()
+        lst = c.ListarPersonas()
+
+        ListBox1.DataSource = lst
+        ListBox1.DisplayMember = "Nombre"
+        ListBox1.ValueMember = "Id"
+    End Sub
+End Class
